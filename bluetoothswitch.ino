954862d7b6ec8838589befc2cc4d50af82535db2
Jason Esteban <81818955+JasonEst-11@users.noreply.github.com>
2021-12-12 14:37:07 +0200

    Update bluetoothswitch.ino

--- bluetoothswitch.ino
+++ bluetoothswitch.ino
@@ -1,10 +1,12 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(9,OUTPUT);
+  pinMode(9,OUTPUT);//relay pin 
 }
 
 void loop() {
+  //check for serial data
   if(serial.available() > 0){
+     // if statements
       if(serial.read() == "on"){
         digitalWrite(9, true);
       }
